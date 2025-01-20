c11dc0c2f4443e4fa2c9c31e57c693e908c24bd9
sreinin <bosreynin7@gmail.com>
2025-01-20 09:42:02 +0700

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Bo Sreinin on 20/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILearningTests/SwiftUILearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningTests.swift
+//  SwiftUILearningTests
+//
+//  Created by Bo Sreinin on 20/1/25.
+//
+
+import Testing
+@testable import SwiftUILearning
+
+struct SwiftUILearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
